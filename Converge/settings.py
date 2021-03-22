f5eff1b9849d6826ddce1280e96430ab01774fad
Sudarshan-PR <psudarshan37@gmail.com>
2021-03-22 18:06:25 +0530

    S3 bucket alter settings

--- Converge/settings.py
+++ Converge/settings.py
@@ -47,9 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+
     'django.contrib.gis',
 
     'home',
@@ -170,15 +169,11 @@ SIMPLE_JWT = {
 }
 
 # AWS S3 buckets settings
-USE_S3 = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_KEY') 
 AWS_STORAGE_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_LOCATION = 'static'
+AWS_S3_REGION_NAME = os.getenv('AWS_REGION_NAME')
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-
-MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+PUBLIC_MEDIA_LOCATION = 'media'
