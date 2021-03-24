beff4bef2e0f8bb38d9eea92f082783e870794e7
Sudarshan-PR <psudarshan37@gmail.com>
2021-03-24 21:03:42 +0530

    Geos lib path

--- Converge/settings.py
+++ Converge/settings.py
@@ -11,9 +11,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# Geos Django library URLs
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
