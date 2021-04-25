03b34359daf9bc6b3f3a07c7f76a14af80ed51ee
Sudarshan-PR <psudarshan37@gmail.com>
2021-04-25 20:23:40 +0530

    Change oneToOne to foriegnKey

--- home/models.py
+++ home/models.py
@@ -5,10 +5,6 @@ from django.contrib.gis.geos import Point
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-# Default value for tags field
-def default_tags():
-        return ['null']
-
 
 # Posts Model
 class Posts(models.Model):
@@ -19,9 +15,9 @@ class Posts(models.Model):
 
 # User Profile Model
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(blank=True)
-    tags = ArrayField(models.CharField(max_length=15), blank=True)
+    tags = ArrayField(models.CharField(max_length=15), blank=True, null=True)
     bio = models.TextField(max_length=100, blank=True)
     dob = models.DateField(blank=True, null=True, auto_now=False, auto_now_add=False)
     location = models.PointField(blank=True)
