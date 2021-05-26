054ee46293010cfef0a636a7dc15eb6fafdcfd22
Sudarshan-PR <psudarshan37@gmail.com>
2021-05-26 22:38:24 +0530

    Get google profile picture

--- register/save_profile_picture.py
+++ register/save_profile_picture.py
@@ -1,10 +1,30 @@
 import home.models
-from PIL import Image
-import requests
+import tempfile
+from django.core import files
 
 def save_profile(backend, user, response, is_new=False, *args, **kwargs):
     if backend.name == 'google-oauth2':
         if is_new and response.get('picture'):
-            image = requests.get(response['picture'], stream=True).raw
-            img = Image.open(image)
-            home.models.Profile.objects.get(user=user).update(image=img)
+            image = requests.get(response['picture'], stream=True)
+
+            if image.status_code != requests.codes.ok:
+                return
+            
+            # Create a temporary file
+            tf = tempfile.NamedTemporaryFile()
+
+            # Read the streamed image in sections
+            for block in image.iter_content(1024 * 8):
+                
+                # If no more file then stop
+                if not block:
+                    break
+
+                # Write image block to temporary file
+                tf.write(block)
+
+            # Create the model you want to save the image to
+            profile = home.models.Profile.objects.get(user=user)
+            
+            profile.image = files.File(tf)
+            profile.save()
