dd215d4030392047b831185931c4b8c367485179
Sudarshan-PR <psudarshan37@gmail.com>
2021-05-26 21:58:59 +0530

    Add auth pipeline

--- Converge/production_settings.py
+++ Converge/production_settings.py
@@ -161,16 +161,15 @@ OAUTH2_PROVIDER = {
 }
 
 SOCIAL_AUTH_PIPELINE = (
-
-    'social.pipeline.social_auth.social_details',
-    'social.pipeline.social_auth.social_uid',
-    'social.pipeline.social_auth.auth_allowed',
-    'social.pipeline.social_auth.social_user',
-    'social.pipeline.user.get_username',
-    'social.pipeline.user.create_user',
-    'social.pipeline.social_auth.associate_user',
-    'social.pipeline.social_auth.load_extra_data',
-    'social.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
     'register.save_profile_picture.save_profile', # This is the path of your pipeline.py
     #and get_avatar is the function.
 )
