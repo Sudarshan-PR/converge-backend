c3be529567f41eba32ec0c1afe780ecf5e52ff3e
Sudarshan-PR <psudarshan37@gmail.com>
2021-05-26 20:11:49 +0530

    Google username conflict test - 2

--- register/serializer.py
+++ register/serializer.py
@@ -15,8 +15,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data['email'],
-            username=validated_data['email'],
             password=validated_data['password'],
+            username=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
         )
--- register/user_manager.py
+++ register/user_manager.py
@@ -4,7 +4,7 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, username, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         '''Create and save a user with the given email, and
         password.
         '''
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
 
         normalized_email = self.normalize_email(email)
 
-        user = self.model(email=normalized_email, username=username, **extra_fields)
+        user = self.model(email=normalized_email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, username, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
