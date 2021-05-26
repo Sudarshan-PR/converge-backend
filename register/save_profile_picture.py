74d3f7f2e73341383989a5cba3e4551e57c2f4ed
Sudarshan-PR <psudarshan37@gmail.com>
2021-05-26 22:03:53 +0530

    Add auth pipeline

--- register/save_profile_picture.py
+++ register/save_profile_picture.py
@@ -5,6 +5,6 @@ import requests
 def save_profile(backend, user, response, is_new=False, *args, **kwargs):
     if backend.name == 'google-oauth2':
         if is_new and response.get('picture'):
-            image = requests.get(requests['picture'], stream=True).raw
+            image = requests.get(response['picture'], stream=True).raw
             img = Image.open(image)
             home.models.Profile.objects.get(user=user).update(image=img)
