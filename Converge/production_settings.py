bdd9bc04936acaee40923af74212cc15b548a2fb
Sudarshan-PR <psudarshan37@gmail.com>
2021-05-26 13:26:41 +0530

    Change authentication backends and classes

--- Converge/production_settings.py
+++ Converge/production_settings.py
@@ -142,9 +142,16 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 
+AUTHENTICATION_BACKENDS = (  
+    'social_core.backends.google.GoogleOAuth2',
+    'drf_social_oauth2.backends.DjangoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  
+        'drf_social_oauth2.authentication.SocialAuthentication',
     )
 }
 
