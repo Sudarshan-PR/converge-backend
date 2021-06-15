b1a0eb96eee87d60a5ae08e53fb5e44e47e380f3
Sudarshan-PR <psudarshan37@gmail.com>
2021-06-15 22:04:14 +0530

    Blank true

--- event/models.py
+++ event/models.py
@@ -12,15 +12,15 @@ class Events(models.Model):
     host = models.ForeignKey(User, on_delete=models.CASCADE)
     attendees = models.ManyToManyField(User, related_name='attending_users')
     invites_sent = models.ManyToManyField(User, related_name='invited_users')
-    title = models.CharField(blank=False, max_length=30)
+    title = models.CharField(blank=True, max_length=30)
     image = models.ImageField(blank=True)
     tags = ArrayField(models.CharField(max_length=15), blank=True, null=True)
     desc = models.TextField(max_length=100, blank=True)
-    create_date = models.DateField(auto_now=True, auto_now_add=False)
-    event_date = models.DateField(auto_now=False, auto_now_add=False)
+    create_date = models.DateField(blank=True, auto_now=True, auto_now_add=False)
+    event_date = models.DateField(blank=True, auto_now=False, auto_now_add=False)
     location = models.PointField(blank=True, null=True)
-    addr = models.TextField(max_length=100)
-    max_attendees = models.IntegerField(default=0)
+    addr = models.TextField(blank=True, max_length=100)
+    max_attendees = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return f'{self.host} : {self.title}'
