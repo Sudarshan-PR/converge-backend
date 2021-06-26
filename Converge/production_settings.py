0fd08abab935c1484e0c1978cb1694627baebadd
Sudarshan-PR <psudarshan37@gmail.com>
2021-06-26 21:19:52 +0530

    Set default schema class

--- Converge/production_settings.py
+++ Converge/production_settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',  
         'drf_social_oauth2.authentication.SocialAuthentication',
-    )
+    ),
 
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
