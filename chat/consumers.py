3d72d6ddfab1c6aefd68c82eacd6855f717bad81
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-04 19:06:12 +0530

    Consumer ignore msg

--- chat/consumers.py
+++ chat/consumers.py
@@ -16,7 +16,6 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
         user = self.scope['user']
 
         self.send(text_data=json.dumps({
