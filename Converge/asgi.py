3a60c45ce4b114c5a630f82f571afadf183a2417
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-04 18:27:48 +0530

    Add channels and setup django

--- Converge/asgi.py
+++ Converge/asgi.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
+import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+
 # import chat.router
 from . import router
 
