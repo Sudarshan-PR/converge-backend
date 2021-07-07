289ede84910dffcc99250de8b08aafe6f26e2e05
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-07 12:55:43 +0530

    event room serializers add

---
+++ chat/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from event.models import Events
+
+class EventRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Events
+        fields = ['id', 'title', 'image', 'host']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id']
