8f6804632f76fb646cde98d19a6c61c9cd7e687f
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-12 20:25:04 +0530

    Notifs models register

--- notification/admin.py
+++ notification/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ExpoToken, UserNotifications
 
 # Register your models here.
+admin.site.register(ExpoToken)
+admin.site.register(UserNotifications)
