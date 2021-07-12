7f0de19a6149f7dd530fc8a273fe6f165be92033
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-12 17:46:53 +0530

    ExpoToken model add

--- notification/models.py
+++ notification/models.py
@@ -7,5 +7,8 @@ User = get_user_model()
 class UserNotifications(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)    
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
-    token = models.CharField(null=True, blank=True)
-    msg = models.CharField(null=True, blank=True)
+    msg = models.CharField(max_length=100, null=True, blank=True)
+
+class ExpoToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=100, null=True, blank=True)
