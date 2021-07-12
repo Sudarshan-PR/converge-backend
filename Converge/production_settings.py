d35908b4d3b5b18ec1eb8af1831dbafe34eb0bbe
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-12 20:11:08 +0530

    Notification app add

--- Converge/production_settings.py
+++ Converge/production_settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'register',
     'event',
     'chat',
+    'notification',
 
     # DRF
     'rest_framework',
@@ -56,6 +57,9 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'social_django',
     'drf_social_oauth2',
+
+    # Notification
+    'exponent_server_sdk',
 
     'puml_generator',
 ]
