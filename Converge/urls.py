321b611e2c0553d464c6f348137c664c98681ec4
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-12 17:51:42 +0530

    Expo token post

--- Converge/urls.py
+++ Converge/urls.py
@@ -1,18 +1,3 @@
-"""Converge URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib.gis import admin
 from django.urls import path, include, re_path
 
@@ -27,6 +12,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from register.views import UserRegisterView, UserVerifyView
+
+from notification.views import storeNotificationTokenView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -46,11 +33,11 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', UserRegisterView.as_view(), name='register_user'),
     path('api/verifyemail/', UserVerifyView.as_view(), name='verify_user'),
 
+    path('api/expotoken/', storeNotificationTokenView, name='expo_token_post'),
+    
     path('api/', include('rest_framework.urls')),
     path('api/', include('drf_social_oauth2.urls',namespace='drf')),
 
--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .serializers import ExpoTokenSerializer
+from .models import ExpoToken
+
+User = get_user_model()
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def storeNotificationTokenView(request):
+    serializer = ExpoTokenSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        data = serializer.validated_data
+        token = ExpoToken(user=request.user, token=data['token'])
+        token.save()
+        
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
