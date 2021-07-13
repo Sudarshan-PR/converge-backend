189e74cfa0fe62c8bf9407aceaacdfb39f903853
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-13 09:47:20 +0530

    User Notif serializer

--- notification/serializers.py
+++ notification/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 
+from .models import UserNotifications
+
 class ExpoTokenSerializer(serializers.Serializer):
     token = serializers.CharField()
-    
+
+class UserNotificationsSerializer(serializers.ModelField):
+    class Meta:
+        model = UserNotifications
+        fields = '__all__'
