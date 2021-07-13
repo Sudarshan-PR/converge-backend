88d14ca7dc5824c859d9d91f513ed3385ff01f8d
Sudarshan-PR <psudarshan37@gmail.com>
2021-07-13 12:01:51 +0530

    Store event image

--- notification/serializers.py
+++ notification/serializers.py
@@ -6,6 +6,14 @@ class ExpoTokenSerializer(serializers.Serializer):
     token = serializers.CharField()
 
 class UserNotificationsSerializer(serializers.ModelField):
+    image = serializers.SerializerMethodField('get_image')
+
+    def get_image(self, obj):
+        if obj.event.image:
+            return obj.event.image.url
+        
+        return None
+
     class Meta:
         model = UserNotifications
         fields = '__all__'
